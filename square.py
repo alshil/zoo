a92914e8bf7826a789a21bafa53ac64c708fd7d2
Alina Shilina <ellyshil@gmail.com>
2017-03-18 18:56:48 +0300

    square formulas for different figures

---
+++ square.py
@@ -0,0 +1,21 @@
+## На вход принимается название фигуры, значения ее сторон, 
+## рассчитывается площадь.
+
+figure_name = str (input("название фигуры "))
+if figure_name == "треугольник":
+	a = float (input ("1 сторона "))
+	b = float (input ("2 сторона "))
+	c = float (input ("3 сторона "))
+	if a + b > c or b + c > a or c + a > b:
+		p = (a + b + c) / 2
+		print (p * (p -a) * (p - b) * (p -c))
+	else:
+		print ("Неверные параметры.Сумма двух сторон всегда больше трейтьей стороны")
+elif figure_name == "прямоугольник":
+	a = float (input ("1 сторона "))
+	b = float (input ("2 сторона "))
+	print (a * b)
+else figure_name == "круг":  ## ругается на эту строчку!
+	radius = float (input("радиус окружности ")
+	pi = 3.14
+	print ((radius ** 2) * pi)
