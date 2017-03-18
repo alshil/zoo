72ae8d95c9737034603bd7b9d9ca0abb52fd5c09
Alina Shilina <ellyshil@gmail.com>
2017-03-18 15:18:48 +0300

    calculator programm

---
+++ year.py
@@ -0,0 +1,34 @@
+a = int (input("первое число "))
+b = str (input("операция "))
+c = int (input("второе число "))
+if b == "+":
+	print (a + c)
+elif b == "-":
+	print (a - c)
+elif b == "/":
+	if c != 0:
+		print (a / c)
+	else:
+		print ("Деление на 0!")	
+		b = int (input ("второе число "))
+		print (a / c)			
+elif b == "*":
+	print (a * c)
+elif b == "mod":
+	if c != 0:
+		print (a % c)
+	else:
+		print ("Деление на 0!")
+		b = int (input ("второе число" ))
+		print (a % c)		
+elif b == "pow":
+	print (a ** c)
+elif b == "dev":
+	if c != 0:
+		print (a // c)
+	else:
+		print ("Деление на 0!")
+		b = int (input ("второе число "))
+		print (a // c)
+		
+	
