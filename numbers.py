1314ef24b98ac8b06eba8a425c8b91bf44b2834d
Alina Shilina <ellyshil@gmail.com>
2017-03-19 14:46:19 +0300

    correction to "numbers.py"

--- numbers.py
+++ numbers.py
@@ -1,29 +1,32 @@
 ## You can enter 3 numbers.
 ## An app will show the bigest number first, 
-## than a smaller one and the smallest.
+## than the smallest one 
+## and a middle in the end.
 
 a = int (input ("Enter the first number "))
 b = int (input ("Enter the second number "))
 c = int (input ("Enter the third number "))
-if a > b and a > c and b > c:
-	print (a + '/n' + b + '/n' + c)
+if c < b < a:
+	print (a)
+	print (b)
+	print (c)
 elif c < a < b:
 	print (b)
-	print (a)
 	print (c)
+	print (a)
 elif b < a < c:
 	print (c)
-	print (a) 
-	print (b)	
+	print (b) 
+	print (a)	
 elif a < b < c:
 	print (c)
-	print (b)
 	print (a)
+	print (b)
 elif a < c < b:
 	print (b)
-	print (c)
 	print (a)
+	print (c)
 else:
 	print (a)
-	print (c)
 	print (b)
+	print (c)
