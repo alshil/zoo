384a82504cbf60b31aa4d4b304e881aa6b37c093
Alina Shilina <ellyshil@gmail.com>
2017-04-23 16:49:33 +0300

    changed 'elif' to 'else' in the last part

--- square.py
+++ square.py
@@ -1,18 +1,21 @@
-# Depends on the figure name an app calculates figure`s square
-figure_name = str (input("figure name "))
+#Depends on the figure name an app calculates figure`s square
+
+import math
+
+figure_name = str(input("figure name "))
 if figure_name == "triangle":
-	a = float (input ("1 side "))
-	b = float (input ("2 side "))
-	c = float (input ("3 sideа "))
+	a = float(input ("1 side "))
+	b = float(input ("2 side "))
+	c = float(input ("3 sideа "))
 	if a + b > c or b + c > a or c + a > b:
 		p = (a + b + c) / 2
-		print (p * (p -a) * (p - b) * (p -c) **0.5 )
+		print(p * (p -a) * (p - b) * (p -c) **0.5)
 	else:
-		print ("Wrong parametrs.A sum of 2 sides is always bigger than a one`s length")
-elif figure_name == "rectangle"
-	a = float (input ("1 side "))
-	b = float (input ("2 side "))
-	print (a * b)
-elif figure_name == "circle": 
-	cir_radius = float (input("radius ")
-	print (((cir_radius) **2) 3.14)
+		print("Wrong parametrs. A sum of 2 sides is always bigger than a one`s length")
+elif figure_name == "rectangle":
+	a = float(input("1 side "))
+	b = float(input("2 side "))
+	print(a * b)
+else: 
+	r = float(input("radius ")
+	print((r ** 2) * math.pi)
